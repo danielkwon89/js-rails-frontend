7b292a46fdb453706e6a9bdc62b6992aeabf9306
danielkwon89 <danielkwon89@gmail.com>
2020-12-15 16:00:44 -0500

    Created login page and choose-a-difficulty page

--- src/index.js
+++ src/index.js
@@ -7,31 +7,61 @@ const easyDifficulty = "https://opentdb.com/api.php?amount=10&category=31&diffic
 const mediumDifficulty = "https://opentdb.com/api.php?amount=10&category=31&difficulty=medium"
 const hardDifficulty = "https://opentdb.com/api.php?amount=10&category=31&difficulty=hard"
 
+// Rails API Endpoints:
+// const players =
+// const scores =
+
 document.addEventListener('DOMContentLoaded', () => {
-    chooseDifficulty()
 
-    let easyButtonClick = document.getElementById('button-easy')
-    easyButtonClick.addEventListener('click', () => {
-        console.log("easy!")
-    })
+    // Load login page
+    // Once 'logged in' run a find_or_create_by in the db with the username
+    // "Log in" the user then run 'chooseDifficulty' and store their user id
+    // Make an API call for the question set for the difficulty chosen then render the quiz
+    // Calculate and display score then run a fetch POST request to create a score associated with the user
+    // Run chooseDifficulty again
 
-    let mediumButtonClick = document.getElementById('button-medium')
-    mediumButtonClick.addEventListener('click', () => {
-        console.log("medium!")
-    })
+    welcomeHeader()
 
-    let hardButtonClick = document.getElementById('button-hard')
-    hardButtonClick.addEventListener('click', () => {
-        console.log("hard!")
+    renderLoginForm()
+
+    // let easyButtonClick = document.getElementById('button-easy')
+    // easyButtonClick.addEventListener('click', () => {
+    //     console.log("easy!")
+    //     fetch(easyDifficulty)
+    //     .then(res => res.json())
+    //     .then(quiz => console.log(quiz))
+    // })
+
+    // let mediumButtonClick = document.getElementById('button-medium')
+    // mediumButtonClick.addEventListener('click', () => {
+    //     console.log("medium!")
+    //     fetch(mediumDifficulty)
+    //     .then(res => res.json())
+    //     .then(quiz => console.log(quiz))
+    // })
+
+    // let hardButtonClick = document.getElementById('button-hard')
+    // hardButtonClick.addEventListener('click', () => {
+    //     console.log("hard!")
+    //     fetch(hardDifficulty)
+    //     .then(res => res.json())
+    //     .then(quiz => console.log(quiz))
+    // })
+
+    let formSubmitButton = document.getElementById("submit-login")
+    formSubmitButton.addEventListener('click', (e) => {
+        e.preventDefault()
+        let userInput = document.getElementById("input-login")
+        findOrCreateUser(userInput.value)
     })
 })
 
-function chooseDifficulty(){
+//
+
+function welcomeHeader(){
     let h1 = document.createElement("h1")
     h1.innerText = "Welcome to OtakQ!"
     document.body.appendChild(h1)
-
-    renderDifficultyButtons()
 }
 
 function renderDifficultyButtons(){
@@ -56,6 +86,93 @@ function renderDifficultyButtons(){
     document.body.appendChild(div)
 }
 
+function renderLoginForm(){
+    let div = document.createElement("div")
+    div.id = "div-login"
+    
+    let form = document.createElement("form")
+    form.id = "form-login"
+
+    let label = document.createElement("label")
+    label.id = "label-login"
+    label.innerText = "Login to Take Quiz:"
+
+    let input = document.createElement("input")
+    input.id = "input-login"
+    input.setAttribute("placeholder", "username")
+
+    let submit = document.createElement("input")
+    submit.id = "submit-login"
+    submit.setAttribute("type", "submit")
+
+    let br1 = document.createElement("br")
+    let br2 = document.createElement("br")
+
+    document.body.appendChild(div)
+    div.appendChild(form)
+    form.appendChild(label)
+    form.appendChild(br1)
+    form.appendChild(input)
+    form.appendChild(br2)
+    form.appendChild(submit)
+}
+
+// let formSubmitButton = document.getElementById("submit-login")
+// formSubmitButton.addEventListener('click' () => 
+// console.log("submit button clicked"))
+
 function renderQuiz(){
 
 }
+
+function findOrCreateUser(username){
+    fetch('http://localhost:3000/api/v1/players', 
+    {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'}, 
+        body: JSON.stringify({player: {name: username}})
+    })
+    .then(res => res.json())
+    .then(obj => {
+
+        let errors = document.getElementsByClassName("div-errors")
+        Array.from(errors).forEach(e => e.remove())
+
+        if (obj.errors){
+
+            let div = document.createElement("div")
+            div.setAttribute("class", "div-errors")
+            let errorList = document.createElement("ul")
+            errorList.id = "ul-errors"
+
+            div.appendChild(errorList)
+            
+            obj.errors.forEach(msg => {
+                let errorMsg = document.createElement("li")
+                errorMsg.innerText = msg
+                
+                errorList.appendChild(errorMsg)
+
+                document.body.appendChild(div)
+            })
+        } else {
+            sessionStorage.setItem("userId", obj.id)
+            sessionStorage.setItem("username", obj.name)
+
+            console.log(sessionStorage.userId)
+            console.log(sessionStorage.username)
+
+            let loginPage = document.getElementById("div-login")
+            loginPage.remove()
+
+            renderDifficultyButtons()
+        }
+    })
+}
+
+// Working fetch POST method
+// fetch('http://localhost:3000/api/v1/players', 
+// {method: 'POST',
+// headers: {'Content-Type': 'application/json'}, 
+// body: JSON.stringify({player: {name: "Jennifer"}})
+// })
